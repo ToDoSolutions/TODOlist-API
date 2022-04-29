c55b1a19da237dbeff8d007f964a6dcb1da27a0c
Alejandro Santiago Félix <alex.0002002@gmail.com>
2022-04-29 21:00:22 +0200

    Template creado.

---
+++ src/main/java/aiss/api/TODOlistApplication.java
@@ -0,0 +1,23 @@
+package aiss.api;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.ws.rs.core.Application;
+
+public class TODOlistApplication extends Application {
+	private Set<Object> singletons = new HashSet<>();
+	// He quitado classes por que no parece útil.
+
+	// Loads all resources that are implemented in the application
+	// so that they can be found by RESTEasy.
+	public TODOlistApplication() {
+		
+		// singletons.add(Class.getInstance());
+	}
+
+	@Override
+	public Set<Object> getSingletons() {
+		return singletons;
+	}
+}
