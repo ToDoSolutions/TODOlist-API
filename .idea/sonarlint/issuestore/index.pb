c62c043da9b5a14f95338928104ce40092694a97
Alejandro Santiago Félix <alex.0002002@gmail.com>
2022-05-06 12:13:52 +0200

    Cambiada presentación de la web.

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,9 @@
+
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+^
+.src/main/java/aiss/api/resources/Resource.java,9\8\988c1749faff77442d77ba566b40dbc1e0aa6fef
+S
+#src/main/java/aiss/model/Model.java,8\a\8a162342d5d1592ba0b64fa2d81d146a86d679e4
--- src/main/java/aiss/api/TODOlistApplication.java
+++ src/main/java/aiss/api/TODOlistApplication.java
@@ -7,17 +7,19 @@ import javax.ws.rs.core.Application;
 
 public class TODOlistApplication extends Application {
 	private Set<Object> singletons = new HashSet<>();
+
 	// He quitado classes por que no parece útil.
 
 	// Loads all resources that are implemented in the application
 	// so that they can be found by RESTEasy.
 	public TODOlistApplication() {
-		
 		// singletons.add(Class.getInstance());
 	}
+
 
 	@Override
 	public Set<Object> getSingletons() {
 		return singletons;
 	}
+
 }
